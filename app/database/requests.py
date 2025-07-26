af3556ef2aeadf3bb6d0f50cce3f322c2d6a0207
Testtimur <timura47@gmail.com>
2025-07-26 13:07:39 +0300

    update db create models

--- app/database/requests.py
+++ app/database/requests.py
@@ -1,7 +1,16 @@
 from app.database.models import async_session
-from app.database.models import User,Item,Category
+from app.database.models import User, Item, Category
 from sqlalchemy import select
 
-async def set_user(tg_id):
+async def set_user(tg_id: int) -> None:
     async with async_session() as session:
         user = await session.scalar(select(User).where(User.tg_id == tg_id))
+
+        if not user:
+            session.add(User(tg_id=tg_id))
+            await session.commit()
+
+
+async def get_categories():
+    async with async_session() as session:
+        return await session.scalars(select(Category))
